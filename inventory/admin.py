34f4f19d18ce0736b8e59be389ba61958e43d6be
Chinmaya24 <chinmaikn@gmail.com>
2025-09-10 18:31:29 +0530

    add

--- inventory/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import User, ListCommodity, SupplierCommodity,Order
-
-
-
-admin.site.register(User)
-admin.site.register(ListCommodity)
-admin.site.register(SupplierCommodity)
-admin.site.register(Order) 
